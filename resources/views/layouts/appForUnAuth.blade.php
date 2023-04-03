27387168121258ad47db478431039980a3bd9055
suou09 <tomjheraldgaufo.labini@bicol-u.edu.ph>
2023-04-04 02:38:31 +0800

    Login button of navbar when user is not authenticated

--- resources/views/layouts/appForUnAuth.blade.php
+++ resources/views/layouts/appForUnAuth.blade.php
@@ -48,102 +48,67 @@
             }
         }
 
-        .door-icon:hover {
-            transform: rotateY(180deg);
-        }
-
-        .door-icon:before {
-            transition: opacity 0.5s ease;
-        }
-
-        .door-icon:hover:before {
-            content: "\f307";
-            font-family: "bootstrap-icons";
-            opacity: 0;
-        }
-
-        .door-icon:hover:before {
-            opacity: 1;
-        }
         .nav-link.dropdown-toggle::before {
             display: none;
+        }
+
+        a.inactive {
+            color: #30455c;
+            transition: color 0.3s ease;
+        }
+    
+        a.inactive:hover{
+            color: #007bff;
+        }
+        
+        a.nav-link.active {
+            color: #007bff !important;
+            border-bottom: 2px solid #007bff !important;
+        }   
+        .textOrange{
+            color: #f1731f !important;
+        }
+
+        .btn-orange{
+            color: white;
+            border: 2px solid #f1731f;
+        }
+
+        .btn-orange:hover{
+            color: black;
+            background: #f1731f;
+            border: 2px solid #f1731f;
         }
     </style>
 
 </head>
-<body class="antialiased">
-    <header class="text-gray-600 body-font">
-        <div class="container-fluid">
-            <div class="row items-center bg-whitetoblue py-3">
-                <div class="col-md-4">
-                    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 mt-4 ms-5">
-                        <img src="{{ asset('media/BU-Carelogo1.png') }}" alt="" class="img-fluid" style="width:35%;">
-                    </a>
-                </div>
-                <div class="col-md-4 text-center">
-                    <nav class="my-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap">
-                        <a class="me-5 transition ease-in-out duration-300 hover:text-slate-50 fs-5" href="#">Home</a>
-                        <a class="me-5 transition ease-in-out duration-300 hover:text-slate-50 fs-5" href="#">Contact</a>
-                        <a class="me-5 transition ease-in-out duration-300 hover:text-slate-50 fs-5" href="#">Set Appointment</a>
-                        <a class="me-1 transition ease-in-out duration-300 hover:text-slate-50 fs-5" href="#">About</a>
-                    </nav>
-                </div>
-                <div class="col-md-4 d-flex justify-content-end">
-                    <nav class="mb-1 md:ml-4 md:py-1 md:pl-4 me-5">
-                        @if (Auth::check())
-                        <li class="nav-item dropdown">
-                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex flex-row">
-                                <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle fs-5" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="white-space: nowrap; color:white;">
-                                            {{ Auth()->user()->first_name }} 
-                                            @if (Auth::user()->hasMedRecord)
-                                                @if (Auth::user()->hasValidatedRecord)
-                                                    <i class="bi bi-patch-check" style="color:#f1731f; margin-top:-2px !important;" data-toggle="tooltip" data-container="body" data-bs-placement="top" title="Validated Medical Record"></i>
-                                                @else
-                                                    <i class="bi bi-file-earmark-medical" style="color:#f1731f; margin-top:30px !important;" data-toggle="tooltip" data-container="body" data-bs-placement="top" title="Submitted Medical Record"></i>
-                                                @endif
-                                            @endif
-                                        </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li>
-                                        <a class="dropdown-item" href="#">Profile</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="dropdown-item" style="{{ Auth::user()->hasMedRecord ? 'display:none;' : '' }}" href="{{ route('medicalForm.show') }}">Submit My Medical Form</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">My Appointments History</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-        
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </li>
-                                </li>
-                            </ul>
-                        </li>
-                        @else
-                        <a href="{{ route('login') }}">
-                            <!--<span><p class="fs-3">Login</p></span> -->
-                            <i class="bi bi-door-closed door-icon fs-2" style="color:#f1731f;" data-toggle="tooltip" data-bs-placement="top" title="Login"></i>
+    <body class="antialiased">
+        <header class="text-gray-600 body-font">
+            <div class="container-fluid">
+                <div class="row items-center bg-whitetoblue py-3">
+                    <div class="col-md-4">
+                        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 mt-4 ms-5" href="{{ route('home') }}">
+                            <img src="{{ asset('media/BU-Carelogo1.png') }}" alt="" class="img-fluid" style="width:35%;">
                         </a>
-                        @endif
-                    </nav>
+                    </div>
+                    <div class="col-md-4 text-center my-auto">
+                        <nav class="my-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap">
+                            <a class="me-5 fs-5 {{ Route::currentRouteName() === 'home' ? 'active' : 'inactive' }} text-decoration-none" href="{{ route('home') }}">Home</a>
+                            <a class="me-5 fs-5 {{ Route::currentRouteName() === 'contacts' ? 'active' : 'inactive' }} text-decoration-none" href="#">Contact</a>
+                            <a class="me-5 fs-5 {{ Route::currentRouteName() === 'appointments' ? 'active' : 'inactive' }} text-decoration-none" href="#">Set Appointment</a>
+                            <a class="me-1 fs-5 {{ Route::currentRouteName() === 'about' ? 'active' : 'inactive' }} text-decoration-none" href="#">About</a>
+                        </nav>
+                    </div>
+                    <div class="col-md-4 d-flex justify-content-end my-auto">
+                        <nav class="mb-1 md:ml-4 md:py-1 md:pl-4 me-5">
+                            <a href="{{ route('login') }}">
+                                <button type="button" class="btn btn-orange rounded">Login</button>
+                            </a>
+                        </nav>
+                    </div>
                 </div>
-      </header>
-
-    @yield('content')
-
-
-    @include('layouts.partial.footer')
-
-
-    
-</body>
+            </div>
+        </header>
+        @yield('content')    
+    </body>
 </html>
