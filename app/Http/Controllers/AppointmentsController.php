f362b74ffb68b368a127cb269a54f28389cb5015
russkyc <jrcc1023@gmail.com>
2023-04-17 21:40:03 +0800

    feat: add appointmentry table migration

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -26,40 +26,37 @@ class AppointmentsController extends Controller
 
     }
 
-   public function appointmentStore(Request $request) {
-    dd($request->all());
-    $res = $request->validate([
-        'appointmentDate' => 'required',
-        'appointmentTime' => 'required',
-        'services' => 'required_if:OthersInput,null',
-        'OthersInput' => 'required_if:services,null',
-        'appointmentDescription' => 'required',
-        
-         ],[
+    public function appointmentStore(Request $request) {
+        $request->validate([
+            'appointmentDate' => 'required',
+            'appointmentTime' => 'required',
+            'services' => 'required_if:OthersInput,null',
+            'OthersInput' => 'required_if:services,null',
+            'appointmentDescription' => 'required',
+        ],[
             'appointmentDate.required' => 'Appointment Date is required',
             'appointmentTime.required' => 'Appointment Time is required',
             'services.required_if' => 'services is required',
             'OthersInput.required_if' => 'Other is required ',
             'appointmentDescription.required' => 'appointmentDescription is required',
         ]);
-dd($res);
-
-            $appointment = new appointment();
-
-            $appointment->appointmentDate = filter_var($request->appointmentDate, FILTER_SANITIZE_STRING);
-            $appointment->appointmentTime = filter_var($request->appointmentTime, FILTER_SANITIZE_STRING);
-            $appointment->appointmentDescription = filter_var($request->appointmentDescription, FILTER_SANITIZE_STRING);
-            
-                    if ($request->services == 'Others') {
-                        $appointment->Others = filter_var($request->Others, FILTER_SANITIZE_STRING);
-                    } else {
-                        $appointment->services = filter_var($request->services, FILTER_SANITIZE_STRING);
-                    }
-                    
-            $appointment->password = hash::make($request->passwordInput);
-            $appointment->save();
-
-            return redirect()->route('setAppointment.show')->with('success', 'Appointment Saved'); 
+    
+        $appointment = new appointment();
+    
+        $appointment->appointmentDate = filter_var($request->appointmentDate, FILTER_SANITIZE_STRING);
+        $appointment->appointmentTime = filter_var($request->appointmentTime, FILTER_SANITIZE_STRING);
+        $appointment->appointmentDescription = filter_var($request->appointmentDescription, FILTER_SANITIZE_STRING);
+                
+        if ($request->services == 'Others') {
+            $appointment->Others = filter_var($request->OthersInput, FILTER_SANITIZE_STRING);
+        } else {
+            $appointment->services = filter_var($request->services, FILTER_SANITIZE_STRING);
+        }
+                        
+        $appointment->save();
+    
+        return redirect()->route('setAppointment.show')->with('success', 'Appointment Saved'); 
     }
+    
 
 }
